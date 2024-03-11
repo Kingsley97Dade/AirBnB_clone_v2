17fd94ca987760cde42aed1d7102af89e6b4d024
Kingsley97Dade <kingsleydade@gmail.com>
2024-03-11 06:36:12 -0700

    Task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,12 +4,12 @@ This module defines a Fabric script that generates a .tgz archive
 from the contents of the web_static folder using the do_pack function.
 """
 
-from fabric import task
+from fabric.api import local, runs_once
 from datetime import datetime
 import os
 
-@task
-def do_pack(c):
+@runs_once
+def do_pack():
     """
     Generates a .tgz archive from the contents of the web_static folder.
 
@@ -17,20 +17,14 @@ def do_pack(c):
         str: The archive path if the archive has been correctly generated,
              otherwise, None.
     """
-    timestamp = datetime.utcnow().strftime('%Y%m%d%H%M%S')
-    archive_name = f"web_static_{timestamp}.tgz"
-    archive_path = os.path.join("versions", archive_name)
+    try:
+        local('mkdir -p versions')
+        time = datetime.now().strftime('%Y%m%d%H%M%S')
+        archive = 'web_static_' + time + '.tgz'
 
-    # Create the versions folder if it doesn't exist
-    if not os.path.exists("versions"):
-        os.makedirs("versions")
+        # generate archive in current directory
+        local(f'tar -cvzf versions/{archive} web_static')
 
-    # Run tar command to create the archive
-    result = c.local(f"tar -cvzf {archive_path} web_static")
-
-    if result.succeeded:
-        print(f"File packed: {archive_path}")
-        return archive_path
-    else:
+        return f'versions/{archive}'
+    except Exception:
         return None
-
